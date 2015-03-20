6a5a978310e8ffbfad14c2a0a6631ef628bdb532
Matt Faulkner <mnfaulk@gmail.com>
2015-03-20 14:59:00 -0700

    Renaming to bolt.py

--- app/bolt.py
+++ app/bolt.py
@@ -1,27 +1,44 @@
 import os
-import time
-from kafka import SimpleProducer, KafkaClient
+from kafka import SimpleProducer, KafkaConsumer, KafkaClient
 from kafka.common import LeaderNotAvailableError
 import logging
 
 logging.basicConfig()
 logger = logging.getLogger('kafka-app')
 
+# Input stream topics
+in_streams = ['test-topic', 'topic-b']
+
 # IP:PORT of a Kafka broker. The typical port is 9092.
 KAFKA_BROKER_IP_PORT = os.getenv('KAFKA_BROKER', '192.168.86.10:9092')
 print "KAFKA BROKER: " + KAFKA_BROKER_IP_PORT
 kafka = KafkaClient(KAFKA_BROKER_IP_PORT)
 producer = SimpleProducer(kafka)
+consumer = KafkaConsumer(*in_streams, group_id="my_group",
+                         metadata_broker_list=[KAFKA_BROKER_IP_PORT])
 
-# Note that the application is responsible for encoding messages to type str
-while True:
-	print('Sending...')
-	logger.info('Sending...')
+
+for message in consumer:
+    # message is raw byte string -- decode if necessary!
+    # e.g., for unicode: `message.decode('utf-8')`
+    execute(message)
+
+# kafka.close()
+
+def execute(message):
+	# IMPLEMENT THIS
+	print message
+
+def emit(topic, msg):
+	"""
+	topic : string
+	msg : string
+	"""
 	try:
-		producer.send_messages("test-topic", "The medium is the message.")
+		producer.send_messages(topic, msg)
 	except LeaderNotAvailableError:
 		logger.warning('Caught a LeaderNotAvailableError. This seems to happen when auto-creating a new topic.')
 		print('Caught a LeaderNotAvailableError. This seems to happen when auto-creating a new topic.')
-	time.sleep(3)
 
-# kafka.close()
+def close():
+	kafka.close()
